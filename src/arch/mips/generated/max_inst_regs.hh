4edd140f05b80599e2292b5555eee5177a31d5ee
Mark Gottscho <mgottscho@ucla.edu>
2015-12-21 16:57:26 -0800

    Changed codebase to be based on what gem5 synthesizes from Python to C++.

---
+++ src/arch/mips/generated/max_inst_regs.hh
@@ -0,0 +1,9 @@
+// DO NOT EDIT
+// This file was automatically generated from an ISA description:
+//   mips/isa/main.isa
+
+namespace MipsISAInst {
+    const int MaxInstSrcRegs = 7;
+    const int MaxInstDestRegs = 4;
+    const int MaxMiscDestRegs = 4;
+}
---
+++ src/arch/types.hh
@@ -0,0 +1,1 @@
+#include "arch/mips/types.hh"
